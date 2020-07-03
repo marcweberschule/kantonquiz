dac5832d582d4bd935a2f99122f44fee55159f40
marweb <theaussieguy99@gmail.com>
2020-07-03 08:44:16 +0200

    quality of life changes

--- app/src/main/java/com/example/kantonquiz/game.java
+++ app/src/main/java/com/example/kantonquiz/game.java
@@ -6,6 +6,7 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.renderscript.Sampler;
 import android.view.View;
@@ -23,6 +24,7 @@ public class game extends AppCompatActivity {
     private Button btnThree;
     private Button btnFour;
     private Button back;
+    private int i = 0;
     private String[] kantone   = {"Zürich","Bern","Luzern","Uri","Schwyz","Nidwalden","Obwalden","Glarus","Zug","Freiburg","Solothurn","Basel-Stadt","Basel-Land","Schaffhausen","AppenzellAusserrhoden","Appenzell Innerhoden","St. Gallen","Graubünden","Aargau","Thurgau","Tessin","Waadt","Wallis","Neuenburg","Genf","Jura"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,7 @@ public class game extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();//test
+                finish();
             }
         });
         this.img = findViewById(R.id.img);
@@ -44,6 +46,15 @@ public class game extends AppCompatActivity {
         int numberOne = randomNumber();
         int numberTwo = randomNumber();
         int numberThree = randomNumber();
+        while (i == numberOne){
+            numberOne = randomNumber();
+        }
+        while (i == numberTwo){
+            numberTwo = randomNumber();
+        }
+        while (i == numberThree){
+            numberThree = randomNumber();
+        }
 
         while (numberOne == numberTwo){
             numberTwo = randomNumber();
@@ -55,40 +66,84 @@ public class game extends AppCompatActivity {
             numberOne = randomNumber();
         }
 
-        animate = AnimationUtils.loadAnimation(this,R.anim.animations);
-        btnOne.startAnimation(animate);
+        /*animate = AnimationUtils.loadAnimation(this,R.anim.animations);
+        btnOne.startAnimation(animate);*/
 
         btnTwo.setText(kantone[numberOne]);
         btnThree.setText(kantone[numberTwo]);
         btnFour.setText(kantone[numberThree]);
+
+        View.OnClickListener btnListenerCor = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ValueAnimator animator = new ValueAnimator();
+                animator.setDuration(300);
+                animator.setEvaluator(new ArgbEvaluator());
+                animator.setIntValues(Color.GRAY,Color.GREEN);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                    public void onAnimationUpdate(ValueAnimator animator){
+                        btnOne.setBackgroundColor((int)animator.getAnimatedValue());
+                    i++;
+                    }
+                });
+                animator.start();
+            }
+        };
         View.OnClickListener btnListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
-                /*colourAnimation(btnTwo);
-                colourAnimation(btnThree);
-                colourAnimation(btnFour);*/
+                final ValueAnimator animator = new ValueAnimator();
+                animator.setDuration(300);
+                animator.setEvaluator(new ArgbEvaluator());
+                animator.setIntValues(Color.GRAY,Color.RED);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                    public void onAnimationUpdate(ValueAnimator animator){
+                        btnTwo.setBackgroundColor((int)animator.getAnimatedValue());
+                    }
+                });
+                animator.start();
             }
         };
-
+        View.OnClickListener btnListenerTwo = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ValueAnimator animator = new ValueAnimator();
+                animator.setDuration(300);
+                animator.setEvaluator(new ArgbEvaluator());
+                animator.setIntValues(Color.GRAY,Color.RED);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                    public void onAnimationUpdate(ValueAnimator animator){
+                        btnThree.setBackgroundColor((int)animator.getAnimatedValue());
+                    }
+                });
+                animator.start();
+            }
+        };
+        View.OnClickListener btnListenerThree = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ValueAnimator valueAnimator = new ValueAnimator();
+                final ValueAnimator animator = new ValueAnimator();
+                animator.setDuration(300);
+                animator.setEvaluator(new ArgbEvaluator());
+                animator.setIntValues(Color.GRAY,Color.RED);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                    public void onAnimationUpdate(ValueAnimator animator){
+                        btnFour.setBackgroundColor((int)animator.getAnimatedValue());
+                    }
+                });
+                animator.start();
+            }
+        };
+        btnOne.setOnClickListener(btnListenerCor);
         btnTwo.setOnClickListener(btnListener);
-        btnThree.setOnClickListener(btnListener);
-        btnFour.setOnClickListener(btnListener);
+        btnThree.setOnClickListener(btnListenerTwo);
+        btnFour.setOnClickListener(btnListenerThree);
 
     }
     private static int randomNumber(){
         int zahl = (int) ((Math.random()*((25-0)+1))+0);
         return zahl;
     }
-    /*private void colourAnimation(View v){
-        int colourStart= v.getSolidColor();
-        int colourEnd = 0x228B22;
-
-        ValueAnimator animate = ObjectAnimator.ofInt(v, "backgroundColor", colourStart,colourEnd);
-        animate.setDuration(2000);
-        animate.setEvaluator(new ArgbEvaluator());
-        animate.setRepeatCount(ValueAnimator.REVERSE);
-        animate.start();
-    }*/
 
 }
